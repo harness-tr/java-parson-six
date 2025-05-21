ce5c344184bbcfaa31019ed601f772197d57a36e
GitHubApp <>
2025-05-21 14:02:48 +0000

    Project init

---
+++ src/main/java/io/harness/Javaparsonsix.java
@@ -0,0 +1,11 @@
+package io.harness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaparsonsix {
+    public static void main(String[] args) {
+        SpringApplication.run(Javaparsonsix.class, args);
+    }
+}
